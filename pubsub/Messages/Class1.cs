4d28a804320c05762725bb6ad1513151038ec161
dylanbeattie <dylan@dylanbeattie.net>
2021-12-18 14:13:06 +0000

    Publisher

---
+++ pubsub/Messages/Class1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Messages {
+    public class DemoMessage {
+        public string Body { get; set; }
+    }
+}
--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using EasyNetQ;
+using Messages;
 
-namespace Publisher
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Publisher {
+    class Program {
+        const string AMQP_URL = "amqps://fmvuwdnx:qGTa9koTU7TzYKxx-eRJquXMI3lAX6Rs@whale.rmq.cloudamqp.com/fmvuwdnx";
+        static void Main(string[] args) {
+            var bus = RabbitHutch.CreateBus(AMQP_URL);
+            var i = 0;
+            while (true) {
+                Console.WriteLine("Press any key to send a message...");
+                Console.ReadKey(false);
+                var body = $"Message #{i++} from {Environment.MachineName} at {DateTime.Now}";
+                var message = new DemoMessage {
+                    Body = body
+                };
+                bus.PubSub.Publish(message);
+                Console.WriteLine($"Published: {body}");
+            }
         }
     }
 }
+
