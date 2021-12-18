3a29fbaadb5c743adc2e3cbd0ac327f17c2e0fe5
dylanbeattie <dylan@dylanbeattie.net>
2021-12-18 09:44:40 +0000

    First working version of pagination on vehicles API controller

--- dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
+++ dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
@@ -3,68 +3,82 @@ using Autobarn.Data.Entities;
 using Autobarn.Website.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Autobarn.Website.Controllers.api {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class VehiclesController : ControllerBase {
-		private readonly IAutobarnDatabase db;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase {
+        private readonly IAutobarnDatabase db;
 
-		public VehiclesController(IAutobarnDatabase db) {
-			this.db = db;
-		}
+        public VehiclesController(IAutobarnDatabase db) {
+            this.db = db;
+        }
 
-		// GET: api/vehicles
-		[HttpGet]
-		public IEnumerable<Vehicle> Get() {
-			return db.ListVehicles();
-		}
 
-		// GET api/vehicles/ABC123
-		[HttpGet("{id}")]
-		public IActionResult Get(string id) {
-			var vehicle = db.FindVehicle(id);
-			if (vehicle == default) return NotFound();
-			return Ok(vehicle);
-		}
+        const int PAGE_SIZE = 10;
+        // GET: api/vehicles
+        [HttpGet]
+        public IActionResult Get(int index = 0) {
+            var items = db.ListVehicles().Skip(index * PAGE_SIZE).Take(PAGE_SIZE);
+            var total = db.CountVehicles();
+            dynamic _links = new ExpandoObject();
+            _links.self = new { href = $"/api/vehicles?index={index}" };
+            if (index > 0) _links.prev = new { href = $"/api/vehicles?index={index - PAGE_SIZE}" };
+            if (index < total) _links.next = new { href = $"/api/vehicles?index={index + PAGE_SIZE}" };
+            var result = new {
+                _links,
+                items
+            };
+            return Ok(result);
+        }
 
-		// POST api/vehicles
-		[HttpPost]
-		public IActionResult Post([FromBody] VehicleDto dto) {
-			var vehicleModel = db.FindModel(dto.ModelCode);
-			var vehicle = new Vehicle {
-				Registration = dto.Registration,
-				Color = dto.Color,
-				Year = dto.Year,
-				VehicleModel = vehicleModel
-			};
-			db.CreateVehicle(vehicle);
-			return Ok(dto);
-		}
+        // GET api/vehicles/ABC123
+        [HttpGet("{id}")]
+        public IActionResult Get(string id) {
+            var vehicle = db.FindVehicle(id);
+            if (vehicle == default) return NotFound();
+            return Ok(vehicle);
+        }
 
-		// PUT api/vehicles/ABC123
-		[HttpPut("{id}")]
-		public IActionResult Put(string id, [FromBody] VehicleDto dto) {
-			var vehicleModel = db.FindModel(dto.ModelCode);
-			var vehicle = new Vehicle {
-				Registration = dto.Registration,
-				Color = dto.Color,
-				Year = dto.Year,
-				ModelCode = vehicleModel.Code
-			};
-			db.UpdateVehicle(vehicle);
-			return Ok(dto);
-		}
+        // POST api/vehicles
+        [HttpPost]
+        public IActionResult Post([FromBody] VehicleDto dto) {
+            var vehicleModel = db.FindModel(dto.ModelCode);
+            var vehicle = new Vehicle {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                VehicleModel = vehicleModel
+            };
+            db.CreateVehicle(vehicle);
+            return Ok(dto);
+        }
 
-		// DELETE api/vehicles/ABC123
-		[HttpDelete("{id}")]
-		public IActionResult Delete(string id) {
-			var vehicle = db.FindVehicle(id);
-			if (vehicle == default) return NotFound();
-			db.DeleteVehicle(vehicle);
-			return NoContent();
-		}
-	}
+        // PUT api/vehicles/ABC123
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] VehicleDto dto) {
+            var vehicleModel = db.FindModel(dto.ModelCode);
+            var vehicle = new Vehicle {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                ModelCode = vehicleModel.Code
+            };
+            db.UpdateVehicle(vehicle);
+            return Ok(dto);
+        }
+
+        // DELETE api/vehicles/ABC123
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id) {
+            var vehicle = db.FindVehicle(id);
+            if (vehicle == default) return NotFound();
+            db.DeleteVehicle(vehicle);
+            return NoContent();
+        }
+    }
 }
