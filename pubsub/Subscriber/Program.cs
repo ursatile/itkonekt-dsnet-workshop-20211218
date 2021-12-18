d30b438a5a23561c5f55640f0a45019c71cad9a9
dylanbeattie <dylan@dylanbeattie.net>
2021-12-18 14:16:42 +0000

    Subscriber

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Messages;
+using EasyNetQ;
 
-namespace Subscriber
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Subscriber {
+    class Program {
+        const string AMQP_URL = "amqps://fmvuwdnx:qGTa9koTU7TzYKxx-eRJquXMI3lAX6Rs@whale.rmq.cloudamqp.com/fmvuwdnx";
+
+        static void Main(string[] args) {
+            using var bus = RabbitHutch.CreateBus(AMQP_URL);
+            bus.PubSub.Subscribe<DemoMessage>("itkonekt-subscriber", message => {
+                Console.WriteLine(message.Body);
+            });
+            Console.WriteLine("Subscriber connected! Listening for messages...");
+            Console.ReadLine();
         }
     }
 }
