723b784c66f328654bac8ad3a3bdf627eb45195c
dylanbeattie <dylan@dylanbeattie.net>
2021-12-19 11:43:19 +0000

    gRPC standalone example

---
+++ grpc/GrpcClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using GrpcDemo;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7037/");
+var grpcClient = new Greeter.GreeterClient(channel);
+Console.WriteLine("Ready! Press a key to send a request");
+
+while (true) {
+    Console.ReadKey(true);
+    var request = new HelloRequest { Name = "ITkonekt" };
+    var reply = grpcClient.SayHello(request);
+    Console.WriteLine(reply.Message);
+}
