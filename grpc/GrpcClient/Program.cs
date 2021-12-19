9a545af6ea03844c7aea629d13ae2df49b0e676b
dylanbeattie <dylan@dylanbeattie.net>
2021-12-19 11:58:20 +0000

    gRPC client/server example

--- grpc/GrpcClient/Program.cs
+++ grpc/GrpcClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using GrpcDemo;
+﻿using System.ComponentModel.Design;
+using GrpcDemo;
 using Grpc.Net.Client;
 
 using var channel = GrpcChannel.ForAddress("https://localhost:7037/");
@@ -6,8 +7,15 @@ var grpcClient = new Greeter.GreeterClient(channel);
 Console.WriteLine("Ready! Press a key to send a request");
 
 while (true) {
-    Console.ReadKey(true);
-    var request = new HelloRequest { Name = "ITkonekt" };
+    Console.WriteLine("Press a key to get a greeting (1 = friendly, 2 = neutral, 3 = hostile");
+    //if (Int32.TryParse(Console.ReadKey().KeyChar.ToString(), out var level)) {
+    var request = new HelloRequest {
+        Name = "ITkonekt",
+        Friendliness = HelloRequest.Types.FriendlinessLevel.Friendly
+    };
     var reply = grpcClient.SayHello(request);
     Console.WriteLine(reply.Message);
+    // } else {
+    //     Console.WriteLine("Unrecognized key");
+    // }
 }
--- grpc/GrpcServer/Services/GreeterService.cs
+++ grpc/GrpcServer/Services/GreeterService.cs
@@ -9,9 +9,17 @@ public class GreeterService : Greeter.GreeterBase {
         _logger = logger;
     }
 
+    static int messageNumber = 0;
+
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context) {
+        messageNumber++;
         return Task.FromResult(new HelloReply {
-            Message = "Hello " + request.Name
+            Message = request.Friendliness switch {
+                HelloRequest.Types.FriendlinessLevel.Friendly => $"Hello, {request.Name}! It is WONDERFUL to see you! Message #{messageNumber}",
+                HelloRequest.Types.FriendlinessLevel.Neutral => $"Hello, {request.Name}. Message #{messageNumber}",
+                HelloRequest.Types.FriendlinessLevel.Hostile => $"Oh no it's, {request.Name}. Message #{messageNumber}",
+                _ => $"(unknown greeting), {request.Name} Message #{messageNumber}"
+            }
         });
     }
 }
