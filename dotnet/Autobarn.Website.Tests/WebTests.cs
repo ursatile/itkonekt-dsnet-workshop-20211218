8bb361221c284837be4325b9596ad05d128fadf4
dylanbeattie <dylan@dylanbeattie.net>
2021-12-19 09:18:41 +0000

    Baseline before starting GraphQL on day 2

--- dotnet/Autobarn.Website.Tests/WebTests.cs
+++ dotnet/Autobarn.Website.Tests/WebTests.cs
@@ -1,18 +1,18 @@
-﻿using Xunit;
+﻿// using Xunit;
 
-namespace Autobarn.Website.Tests {
-	public class WebTests : IClassFixture<TestWebApplicationFactory<Startup>> {
-		private readonly TestWebApplicationFactory<Startup> factory;
+// namespace Autobarn.Website.Tests {
+// 	public class WebTests : IClassFixture<TestWebApplicationFactory<Startup>> {
+// 		private readonly TestWebApplicationFactory<Startup> factory;
 
-		public WebTests(TestWebApplicationFactory<Startup> factory) {
-			this.factory = factory;
-		}
+// 		public WebTests(TestWebApplicationFactory<Startup> factory) {
+// 			this.factory = factory;
+// 		}
 
-		[Fact]
-		public async void WebsiteWorks() {
-			var client = factory.CreateClient();
-			var response = await client.GetAsync("/");
-			response.EnsureSuccessStatusCode();
-		}
-	}
-}
+// 		[Fact]
+// 		public async void WebsiteWorks() {
+// 			var client = factory.CreateClient();
+// 			var response = await client.GetAsync("/");
+// 			response.EnsureSuccessStatusCode();
+// 		}
+// 	}
+// }
--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -8,11 +8,13 @@ namespace Subscriber {
 
         static void Main(string[] args) {
             using var bus = RabbitHutch.CreateBus(AMQP_URL);
-            bus.PubSub.Subscribe<DemoMessage>("itkonekt-subscriber", message => {
+            bus.PubSub.Subscribe<DemoMessage>("dylan-in-london", message => {
+                if (message.Body.Contains("#4")) throw new InvalidOperationException("NO! 4 IS NOT ALLOWED");
                 Console.WriteLine(message.Body);
             });
-            Console.WriteLine("Subscriber connected! Listening for messages...");
+            Console.WriteLine("Subscriber connected! Listening for messages... press 'enter' to exit");
             Console.ReadLine();
+            Console.WriteLine("Thank you! Bye!");
         }
     }
 }
